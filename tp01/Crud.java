e8c6be1c545d0545e9cdc9147234d77321486557
Felipefams <augustofams@gmail.com>
2022-10-07 07:48:52 -0300

    crud

--- tp01/Crud.java
+++ tp01/Crud.java
@@ -1,3 +1,33 @@
+import java.io.RandomAccessFile;
+import java.io.IOException;
+import java.util.Scanner;
+import User;
 public class Crud {
-    
+	public static void main(String[] args) {
+		User i = new User();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name");
+		i.setName(sc.nextLine());
+		System.out.println("Enter the age");
+		i.setAge(sc.nextInt());
+		System.out.println("Enter the address");
+		i.setAddress(sc.nextLine());
+		System.out.println("Enter the phone number");
+		i.setPhone(sc.nextLine());
+		System.out.println("Enter the email");
+		i.setEmail(sc.nextLine());
+		System.out.println("Enter the password");
+		i.setPassword(sc.nextLine());
+	}	
+	public void create(User i) {
+		try {
+			RandomAccessFile raf = new RandomAccessFile("user.txt", "rw");
+			raf.seek(raf.length());
+			raf.writeBytes(i.getName() + " " + i.getAge() + " " + i.getAddress() + " " + i.getPhone() + 
+					" " + i.getEmail() + " " + i.getPassword() + " " + i.getRole() + " " + i.getBalance());
+			raf.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}	
 }
---
+++ tp01/File.java
@@ -0,0 +1,6 @@
+import randomAccessFile;
+public class File {
+	public static void main(String[] args) {
+		//hello world
+	}
+}
