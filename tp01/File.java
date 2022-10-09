5ccaf50125aa08d66e259e78dda147a7b180c572
Felipefams <augustofams@gmail.com>
2022-10-09 17:43:17 -0300

    old

--- tp01/File.java
+++ tp01/File.java
@@ -1,18 +1,23 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 
 public class File {
 	public static void main(String[] args) throws FileNotFoundException {
+		ArrayList<String> emails = new ArrayList<String>();
+		emails.add("teste@gmail.com");
+		User u = new User(1, "Joao", emails, "teste", "teste", "teste", "teste", 1, 1F);
 		try {
 			RandomAccessFile raf = new RandomAccessFile("teste.db", "rw");
-			raf.writeBytes("teste");
+			raf.write(u.toByteArray());
+			// raf.writeBytes("teste");
 			long pos = raf.getFilePointer();
 			System.out.println("Registro iniciado na posição: "+pos);
-			int k = 10;
-			byte x = Byte.parseByte(Integer.toString(k));
-			raf.writeInt(k);
-			raf.write(x);
+			while(raf.getFilePointer() < raf.length()) {
+
+			}
+			raf.close();
 		}catch (IOException e) {
 			throw new RuntimeException(e);
 		}
--- tp01/User.java
+++ tp01/User.java
@@ -1,3 +1,9 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -10,12 +16,12 @@ public class User {
     protected ArrayList<String> emails;
     protected String nomeUsuario;
     protected String senha;
-    protected char[] cpf = new char[11];
+    protected String cpf;// = new char[11];
     protected String cidade;
     protected int transferenciasRealizadas;
     protected float saldoConta;
 
-    public User(int idConta, String nomePessoa, ArrayList<String> emails, String nomeUsuario, String senha, char[] cpf, String cidade, int transferenciasRealizadas, float saldoConta) {
+    public User(int idConta, String nomePessoa, ArrayList<String> emails, String nomeUsuario, String senha, String cpf, String cidade, int transferenciasRealizadas, float saldoConta) {
         this.idConta = idConta;
         this.nomePessoa = nomePessoa;
         this.emails = emails;
@@ -67,11 +73,11 @@ public class User {
         this.senha = senha;
     }
 
-    public char[] getCpf() {
+    public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(char[] cpf) {
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
@@ -82,7 +88,7 @@ public class User {
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
-    
+
     public int getTransferenciasRealizadas() {
         return transferenciasRealizadas;
     }
@@ -94,4 +100,33 @@ public class User {
     public float getSaldoConta() {
         return saldoConta;
     }
+
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutput dos = new DataOutputStream(baos);
+        dos.writeInt(idConta);
+        dos.writeUTF(nomePessoa);
+        dos.writeUTF(emails.toString());
+        dos.writeUTF(nomeUsuario);
+        dos.writeUTF(senha);
+        dos.writeUTF(cpf);
+        dos.writeUTF(cidade);
+        dos.writeInt(transferenciasRealizadas);
+        dos.writeFloat(saldoConta);
+        return baos.toByteArray();
+    }
+
+    public void fromByteArray(byte[] bytes) throws IOException {
+        DataInputStream dis = new DataInputStream(new ByteArrayInputStream(bytes));
+        idConta = dis.readInt();
+        nomePessoa = dis.readUTF();
+        for(int i = 0; i < emails.size(); ++i) emails.set(i, dis.readUTF()); 
+        nomeUsuario = dis.readUTF();
+        senha = dis.readUTF();
+        cpf = dis.readUTF();
+        cidade = dis.readUTF();
+        transferenciasRealizadas = dis.readInt();
+        saldoConta = dis.readFloat();
+    }
+
 }
