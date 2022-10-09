2a006eae4c51b44c516682188553f0dc15ea9909
Felipefams <augustofams@gmail.com>
2022-10-09 15:38:58 -0300

    tp01

--- tp01/File.java
+++ tp01/File.java
@@ -1,6 +1,19 @@
-import randomAccessFile;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class File {
-	public static void main(String[] args) {
-		//hello world
+	public static void main(String[] args) throws FileNotFoundException {
+		try {
+			RandomAccessFile raf = new RandomAccessFile("teste.db", "rw");
+			long pos = raf.getFilePointer();
+			System.out.println("Registro iniciado na posição: "+pos);
+			int k = 10;
+			byte x = Byte.parseByte(Integer.toString(k));
+			raf.writeInt(k);
+			raf.write(x);
+		}catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
